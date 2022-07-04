e0c218ae5840228d4f2d8893a0f259fb28af910b
maicatheus <maicatheus@gmail.com>
2022-07-04 19:29:40 -0300

    Project created

---
+++ src/main/java/com/example/stscrud/StscrudApplication.java
@@ -0,0 +1,13 @@
+package com.example.stscrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StscrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StscrudApplication.class, args);
+	}
+
+}
