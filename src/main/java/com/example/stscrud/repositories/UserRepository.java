37edff7bcefc07afdeada233d26e75106d0d7235
maicatheus <maicatheus@gmail.com>
2022-07-04 20:31:18 -0300

    JPA repository, dependency injection, database seeding

---
+++ src/main/java/com/example/stscrud/repositories/UserRepository.java
@@ -0,0 +1,12 @@
+package com.example.stscrud.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.stscrud.entities.User;
+
+public interface UserRepository extends JpaRepository<User, Long>{
+	
+	
+	
+	
+}
