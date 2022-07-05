294d30a2ebbfd2a3e7df6e7939eb58f622d3a04f
maicatheus <maicatheus@gmail.com>
2022-07-04 21:09:18 -0300

    Service layer, component registration

--- src/main/java/com/example/stscrud/repositories/UserRepository.java
+++ src/main/java/com/example/stscrud/repositories/UserRepository.java
@@ -1,9 +1,11 @@
 package com.example.stscrud.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.example.stscrud.entities.User;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long>{
 	
 	
